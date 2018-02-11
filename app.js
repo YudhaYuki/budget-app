190d4780124d3b109255a405db0a866df5c8723c
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-11 16:02:58 +0100

    Module - controller

--- app.js
+++ app.js
@@ -8,8 +8,30 @@ var budgetController = (function() {
 
     return {
         publicTest: function(b) {
-            console.log(add(b));
+            return add(b);
         }
     }
 
 })();
+
+
+var UIController = (function() {
+
+    // Some code
+
+})();
+
+// Module can also receive argument
+// Remember they are just function expressions
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
