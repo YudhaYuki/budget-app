9bda719aa23a5dbd043d144879397ab8e2a06db2
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-11 21:02:08 +0100

    Adding new items to the UI --- Adding big chunks of HTML into DOM Part II

--- app.js
+++ app.js
@@ -115,20 +115,23 @@ var UIController = (function() {
         // 
         addListItem: function(obj, type) {
 
-            var html;
+            var html, newHtml;
 
             // Create HTML string with placeholder text
 
             if (type === 'inc') {
-                html = '<div class="item clearfix" id="income-0"> <div class="item__description">Salary</div> <div class="right clearfix"> <div class="item__value">+ 2,100.00</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'; 
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'; 
             } else if (type === 'exp') {
-                html = '<div class="item clearfix" id="expense-0"> <div class="item__description">Apartment rent</div> <div class="right clearfix"> <div class="item__value">- 900.00</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
             }
 
-
             // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
 
             // Insert the HTML into the DOM
+            
         },
 
         // Basically, we are now exposing the DOMstrings object into the public
