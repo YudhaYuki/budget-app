314d4978198eea06cef1bdfd57f3d26250815dad
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-11 17:02:57 +0100

    Make the three inputs WORK when we hit enter/button

--- app.js
+++ app.js
@@ -12,8 +12,16 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
 
-    // Some code
-
+    return {
+        getinput: function() {
+            // Method for returning all of the three inputs that we had in the user interface
+            return {
+                type : document.querySelector('.add__type').value, // Will be either inc or exp
+                description : document.querySelector('.add__description').value,
+                value : document.querySelector('.add__value').value
+            };
+        }
+    };
 })();
 
 
@@ -30,6 +38,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -39,7 +49,6 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
 
-        console.log('it works.');
     }
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
