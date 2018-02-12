cd5d360aaa3eb8676fbf950c20a909b355298ac4
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-12 16:22:47 +0100

    Fix bug - Changing string to integer, because we couldn't delet the items income BY USING PARSEINT

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete the item from the data structure
             budgetCtrl.deleteItem(type, ID);
