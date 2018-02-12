fa294f46b4238e9ffcc4073d82a6537af86909d7
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-12 20:37:35 +0100

    Update percentages part II - nodeListForEach method

--- app.js
+++ app.js
@@ -217,7 +217,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
 
 
@@ -300,6 +301,27 @@ var UIController = (function() {
             } else {
                 document.querySelector(DOMstrings.percentageLabel).textContent = '---';                  
             }
+        },
+
+        displayPercentages: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index) {
+
+                if(percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';                    
+                }
+            });
+            
         },
 
         // Basically, we are now exposing the DOMstrings object into the public
